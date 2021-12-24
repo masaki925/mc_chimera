8a48b461f6c75437510dc56584a62ce7229e9731
masaki925 <masaki925@gmail.com>
2021-12-25 06:15:07 +0900

    clean code

--- src/mc_chimera/main.py
+++ src/mc_chimera/main.py
@@ -2,13 +2,13 @@ import os
 from fastapi import FastAPI, request, abort
 
 from linebot import (
-　LineBotApi, WebhookHandler
+  LineBotApi, WebhookHandler
 )
 from linebot.exceptions import (
-　InvalidSignatureError
+  InvalidSignatureError
 )
 from linebot.models import (
-　MessageEvent, TextMessage, TextSendMessage
+  MessageEvent, TextMessage, TextSendMessage
 )
 
 
@@ -23,19 +23,20 @@ handler = WebhookHandler(SECRET)
 
 @app.route(“/callback”, methods=[‘POST’])
 def callback():
-  signature = request.headers[‘X-Line-Signature’] 　body = request.get_data(as_text=True)
+  signature = request.headers[‘X-Line-Signature’]
+  body = request.get_data(as_text=True)
   app.logger.info(“Request body: ” + body)
 
   try:
-  　handler.handle(body, signature)
+    handler.handle(body, signature)
   except InvalidSignatureError:
-  　abort(400)
+    abort(400)
 
   return ‘OK’
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-　line_bot_api.reply_message(
-　　event.reply_token,
-　　TextSendMessage(text=event.message.text))
+  line_bot_api.reply_message(
+    event.reply_token,
+    TextSendMessage(text=event.message.text))
 
